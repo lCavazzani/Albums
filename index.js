c494a138381947717d5f7d6b071eae001ea68e53
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-06-09 14:39:35 -0300

    create and use Header component

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import Header from './src/components/header';
+
+const App = () => (
+        <Header />
+    );
+// Pre-refactor
+// const App = () => {  
+//     return (
+//         <Text>Udemy Test</Text>
+//     );
+// };
 
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => (
+    <Text> Albums! </Text>
+);
+// Pre Refactor
+// const Header = () => {
+//     return (
+//         <Text> Albums! </Text>
+//     );
+// };
+export default Header; 
