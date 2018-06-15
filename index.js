c95f057fc5f31df6383ab90c19eeda0d45ddfdfc
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-06-14 23:46:12 -0300

    fetch in componentWillMount

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import React from 'react';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => 
 (
+    <View>
         <Header headerText='Albums!' />
+        <AlbumList />
+    </View>
     );
 // Pre-refactor
 // so funciona se for simples, nao consegui criar variaveis dentro da funcao assim
