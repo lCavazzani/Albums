603f216296439a944bac55a0c2b2de7bb6d4bc86
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-06-18 00:51:54 -0300

    the second album detail with image of album

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,32 +3,58 @@ import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => (
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image } = album;
+    const { thumbnailStyle, headerContentStyle, 
+            thumbnailtContainerStyle, headerTextStyle, imageStyle } = styles;
+    return (
         <View>
             <Card>
-                <CardSection style={styles.cardHeader}>
-                    <View>
-                        <Text> {props.album.title} </Text>
+                <CardSection>
+                    <View style={thumbnailtContainerStyle}>
+                        <Image 
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }} 
+                        />
                     </View>
-                    <View style={styles.cardHeaderText}>
-                        <Text> {props.album.title} </Text>
-                        <Text> {props.album.artist} </Text>
+                    <View style={headerContentStyle}>
+                        <Text style={headerTextStyle}> {title} </Text>
+                        <Text> {artist} </Text>
                     </View>
                 </CardSection>
-                
+                <CardSection>
+                    <Image source={{ uri: image }} style={imageStyle} />
+                </CardSection>
             </Card>
         </View>
     );
+};
 
 const styles = {
-    cardHeader: {
-        justifyContent: 'flex-start',
-        flexDirection: 'row',
-    },
-    cardHeaderText: {
+    headerContentStyle: {
         justifyContent: 'space-around',
-        flexDirection: 'column'
-    }
+        flexDirection: 'column',
+        padding: 5
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailtContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    }, 
+    imageStyle: { 
+        height: 300,
+        flex: 1,
+        width: null
+    },
+
 };
 //I'm just using this Card component for styling purpose. I'm nesting the elements inside the Card.
 export default AlbumDetail; 
